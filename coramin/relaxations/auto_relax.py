1447b97c9fcf1aadf7b6bf942d1cd738884783cb
Michael Bynum <michaelbynum@users.noreply.github.com>
2019-06-05 09:43:35 -0600

    identifying x*x as x**2 in auto_relax

--- coramin/relaxations/auto_relax.py
+++ coramin/relaxations/auto_relax.py
@@ -73,6 +73,20 @@ def _relax_leaf_to_root_ProductExpression(node, values, aux_var_map, degree_map,
     # aux1 = x*y
     # aux2 = x*y
     #
+    if arg1 is arg2:
+        if degree_map[arg1] == 0:
+            res = arg1 * arg2
+            degree_map[res] = 0
+            return res
+        else:
+            # reformulate arg1 * arg2 as arg1**2
+            _new_relaxation_side_map = ComponentMap()
+            _reformulated = arg1 ** 2
+            _new_relaxation_side_map[_reformulated] = relaxation_side_map[node]
+            res = _relax_expr(expr=_reformulated, aux_var_map=aux_var_map, parent_block=parent_block,
+                              relaxation_side_map=_new_relaxation_side_map, counter=counter)
+            degree_map[res] = 1
+            return res
 
     if arg1.__class__ == numeric_expr.MonomialTermExpression:
         coef, arg1 = arg1.args
