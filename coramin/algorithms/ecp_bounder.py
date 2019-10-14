706a29bab88d46e1818b9ae08c3f785a2e08f0fb
Michael Bynum <michaelbynum@users.noreply.github.com>
2019-10-14 09:09:02 -0600

    adding tests for ECPBounder

--- coramin/algorithms/ecp_bounder.py
+++ coramin/algorithms/ecp_bounder.py
@@ -1,4 +1,5 @@
 from pyomo.solvers.plugins.solvers.persistent_solver import PersistentSolver
+from pyomo.opt.base.solvers import OptSolver
 from pyomo.core.kernel.component_set import ComponentSet
 from coramin.relaxations.relaxations_base import BaseRelaxationData, BaseRelaxation
 import pyomo.environ as pe
@@ -11,15 +12,16 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class ECPBounder(PersistentSolver):
+class _ECPBounder(OptSolver):
     """
-    A solver designed for use inside of OBBT. This solver is a persistent solver for efficient changes to the
-    objective. Additionally, it provides a mechanism for refining convex nonlinear constraints during OBBT.
+    This class is used for testing
     """
-    def __init__(self, subproblem_solver='gurobi_persistent'):
+    def __init__(self, subproblem_solver):
         self._subproblem_solver = pe.SolverFactory(subproblem_solver)
-        if not isinstance(self._subproblem_solver, PersistentSolver):
-            raise ValueError('subproblem_solver must be a persistent solver')
+        if isinstance(self._subproblem_solver, PersistentSolver):
+            self._using_persistent_solver = True
+        else:
+            self._using_persistent_solver = False
         self._relaxations = ComponentSet()
         self._relaxations_not_tracking_solver = ComponentSet()
         self._relaxations_with_added_cuts = ComponentSet()
@@ -35,37 +37,12 @@ class ECPBounder(PersistentSolver):
 
         self.subproblem_solver_options = ConfigBlock(implicit=True)
 
-    def set_instance(self, model, **kwargs):
-        self._pyomo_model = model
-        self._subproblem_solver.set_instance(model, **kwargs)
-
-    def add_block(self, block):
-        self._subproblem_solver.add_block(block)
-
-    def add_constraint(self, con):
-        self._subproblem_solver.add_constraint(con)
-
-    def add_var(self, var):
-        self._subproblem_solver.add_var(var)
-
-    def remove_block(self, block):
-        self._subproblem_solver.remove_block(block)
-
-    def remove_constraint(self, con):
-        self._subproblem_solver.remove_constraint(con)
-
-    def remove_var(self, var):
-        self._subproblem_solver.remove_var(var)
-
-    def set_objective(self, obj):
-        self._subproblem_solver.set_objective(obj)
-
-    def update_var(self, var):
-        self._subproblem_solver.update_var(var)
-
-    def solve(self, **kwargs):
+    def solve(self, *args, **kwargs):
         t0 = time.time()
         logger.info('{0:<10}{1:<12}{2:<12}{3:<12}{4:<12}'.format('Iter', 'objective', 'max_viol', 'time', '# cuts'))
+
+        if not self._using_persistent_solver:
+            self._pyomo_model = args[0]
 
         options = self.options(kwargs.pop('options', dict()))
         subproblem_solver_options = self.subproblem_solver_options(kwargs.pop('subproblem_solver_options', dict()))
@@ -86,12 +63,16 @@ class ECPBounder(PersistentSolver):
         for b in self._pyomo_model.component_data_objects(pe.Block, descend_into=True, active=True, sort=True):
             if isinstance(b, (BaseRelaxationData, BaseRelaxation)):
                 self._relaxations.add(b)
-                if self not in b._persistent_solvers:
-                    b.add_persistent_solver(self)
-                    self._relaxations_not_tracking_solver.add(b)
+                if self._using_persistent_solver:
+                    if self not in b._persistent_solvers:
+                        b.add_persistent_solver(self)
+                        self._relaxations_not_tracking_solver.add(b)
 
         for _iter in range(options.max_iter):
-            res = self._subproblem_solver.solve(save_results=False, options=subproblem_solver_options)
+            if self._using_persistent_solver:
+                res = self._subproblem_solver.solve(save_results=False, options=subproblem_solver_options)
+            else:
+                res = self._subproblem_solver.solve(self._pyomo_model, options=subproblem_solver_options)
             if res.solver.termination_condition != pe.TerminationCondition.optimal:
                 final_res.solver.termination_condition = pe.TerminationCondition.other
                 final_res.solver.status = pe.SolverStatus.aborted
@@ -139,11 +120,51 @@ class ECPBounder(PersistentSolver):
             for b in self._relaxations_with_added_cuts:
                 b.rebuild()
 
-        for b in self._relaxations_not_tracking_solver:
-            b.remove_persistent_solver(self)
+        if self._using_persistent_solver:
+            for b in self._relaxations_not_tracking_solver:
+                b.remove_persistent_solver(self)
 
         final_res.solver.wallclock_time = time.time() - t0
         return final_res
+
+
+class ECPBounder(_ECPBounder, PersistentSolver):
+    """
+    A solver designed for use inside of OBBT. This solver is a persistent solver for efficient changes to the
+    objective. Additionally, it provides a mechanism for refining convex nonlinear constraints during OBBT.
+    """
+    def __init__(self, subproblem_solver):
+        super(ECPBounder, self).__init__(subproblem_solver=subproblem_solver)
+        if not self._using_persistent_solver:
+            raise ValueError('subproblem solver must be a persistent solver')
+
+    def set_instance(self, model, **kwargs):
+        self._pyomo_model = model
+        self._subproblem_solver.set_instance(model, **kwargs)
+
+    def add_block(self, block):
+        self._subproblem_solver.add_block(block)
+
+    def add_constraint(self, con):
+        self._subproblem_solver.add_constraint(con)
+
+    def add_var(self, var):
+        self._subproblem_solver.add_var(var)
+
+    def remove_block(self, block):
+        self._subproblem_solver.remove_block(block)
+
+    def remove_constraint(self, con):
+        self._subproblem_solver.remove_constraint(con)
+
+    def remove_var(self, var):
+        self._subproblem_solver.remove_var(var)
+
+    def set_objective(self, obj):
+        self._subproblem_solver.set_objective(obj)
+
+    def update_var(self, var):
+        self._subproblem_solver.update_var(var)
 
     def load_vars(self, vars_to_load=None):
         self._subproblem_solver.load_vars(vars_to_load=vars_to_load)
---
+++ coramin/algorithms/tests/test_ecp_bounder.py
@@ -0,0 +1,45 @@
+import pyomo.environ as pe
+import coramin
+from coramin.algorithms.ecp_bounder import _ECPBounder
+import unittest
+import logging
+try:
+    import gurobipy
+    gurobipy_available = True
+except ImportError:
+    gurobipy_available = False
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+class TestECPBounder(unittest.TestCase):
+    def test_ecp_bounder(self):
+        m = pe.ConcreteModel()
+        m.x = pe.Var()
+        m.y = pe.Var()
+        m.obj = pe.Objective(expr=0.5*(m.x**2 + m.y**2))
+        m.c1 = pe.Constraint(expr=m.y >= (m.x - 1)**2)
+        m.c2 = pe.Constraint(expr=m.y >= pe.exp(m.x))
+        coramin.relaxations.relax(m, in_place=True)
+        opt = _ECPBounder(subproblem_solver='glpk')
+        res = opt.solve(m)
+        self.assertEqual(res.solver.termination_condition, pe.TerminationCondition.optimal)
+        self.assertAlmostEqual(m.x.value, 0)
+        self.assertAlmostEqual(m.y.value, 1)
+
+    @unittest.skipIf(not gurobipy_available, reason='persistent solver not available')
+    def test_ecp_bounder_persistent(self):
+        m = pe.ConcreteModel()
+        m.x = pe.Var()
+        m.y = pe.Var()
+        m.obj = pe.Objective(expr=0.5*(m.x**2 + m.y**2))
+        m.c1 = pe.Constraint(expr=m.y >= (m.x - 1)**2)
+        m.c2 = pe.Constraint(expr=m.y >= pe.exp(m.x))
+        coramin.relaxations.relax(m, in_place=True)
+        opt = coramin.algorithms.ECPBounder(subproblem_solver='gurobi_persistent')
+        opt.set_instance(m)
+        res = opt.solve()
+        self.assertEqual(res.solver.termination_condition, pe.TerminationCondition.optimal)
+        self.assertAlmostEqual(m.x.value, 0)
+        self.assertAlmostEqual(m.y.value, 1)
