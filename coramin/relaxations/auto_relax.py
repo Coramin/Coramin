acce3775b93571f5c7a7526ffd8b56e341ea59ee
Michael Bynum <michaelbynum@users.noreply.github.com>
2020-04-15 11:56:05 -0600

    bug

--- coramin/relaxations/auto_relax.py
+++ coramin/relaxations/auto_relax.py
@@ -85,7 +85,17 @@ def _relax_leaf_to_root_ProductExpression(node, values, aux_var_map, degree_map,
     # aux2 = x*y
     #
 
-    if arg1.__class__ == numeric_expr.MonomialTermExpression or arg2.__class__ == numeric_expr.MonomialTermExpression:
+    degree_1 = degree_map[arg1]
+    degree_2 = degree_map[arg2]
+    if degree_1 == 0:
+        res = arg1 * arg2
+        degree_map[res] = degree_2
+        return res
+    elif degree_2 == 0:
+        res = arg2 * arg1
+        degree_map[res] = degree_1
+        return res
+    elif arg1.__class__ == numeric_expr.MonomialTermExpression or arg2.__class__ == numeric_expr.MonomialTermExpression:
         if arg1.__class__ == numeric_expr.MonomialTermExpression:
             coef1, arg1 = arg1.args
         else:
@@ -101,17 +111,6 @@ def _relax_leaf_to_root_ProductExpression(node, values, aux_var_map, degree_map,
         res = _relax_expr(expr=_reformulated, aux_var_map=aux_var_map, parent_block=parent_block,
                           relaxation_side_map=_new_relaxation_side_map, counter=counter, degree_map=degree_map)
         degree_map[res] = 1
-        return res
-
-    degree_1 = degree_map[arg1]
-    degree_2 = degree_map[arg2]
-    if degree_1 == 0:
-        res = arg1 * arg2
-        degree_map[res] = degree_2
-        return res
-    elif degree_2 == 0:
-        res = arg2 * arg1
-        degree_map[res] = degree_1
         return res
     elif arg1 is arg2:
         # reformulate arg1 * arg2 as arg1**2
