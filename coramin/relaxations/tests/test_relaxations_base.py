5be8ab9bff7765f0b59aa5d263c697fb82b8f096
Michael Bynum <michaelbynum@users.noreply.github.com>
2020-07-05 15:09:47 -0600

    adding test for push and pop partitions

--- coramin/relaxations/tests/test_relaxations_base.py
+++ coramin/relaxations/tests/test_relaxations_base.py
@@ -56,3 +56,32 @@ class TestBaseRelaxation(unittest.TestCase):
         assert_optimal_termination(res)
         self.assertAlmostEqual(m.x.value, 0)
         self.assertAlmostEqual(m.y.value, -1)
+
+    def test_push_and_pop_partitions(self):
+        m = pe.ConcreteModel()
+        m.x = pe.Var(bounds=(-2, 1))
+        m.y = pe.Var()
+        m.rel = coramin.relaxations.PWXSquaredRelaxation()
+        m.rel.build(x=m.x, aux_var=m.y)
+        m.obj = pe.Objective(expr=m.y)
+        self.assertEqual(m.rel._partitions[m.x], [-2, 1])
+
+        m.rel.add_partition_point(-1)
+        m.rel.rebuild()
+        self.assertEqual(m.rel._partitions[m.x], [-2, -1, 1])
+
+        m.rel.push_partitions()
+        m.rel.rebuild()
+        self.assertEqual(m.rel._partitions[m.x], [-2, -1, 1])
+
+        m.rel.clear_partitions()
+        m.rel.rebuild()
+        self.assertEqual(m.rel._partitions[m.x], [-2, 1])
+
+        m.rel.add_partition_point(-0.5)
+        m.rel.rebuild()
+        self.assertEqual(m.rel._partitions[m.x], [-2, -0.5, 1])
+
+        m.rel.pop_partitions()
+        m.rel.rebuild()
+        self.assertEqual(m.rel._partitions[m.x], [-2, -1, 1])
